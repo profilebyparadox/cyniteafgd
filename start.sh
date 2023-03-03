611da327811c2c18e6ca017309cfca846a6a3f6b
rohitjangra11 <126782133+rohitjangra11@users.noreply.github.com>
2023-03-03 16:51:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /File
 fi
-cd /Shortner
+cd /File
 pip3 install -U -r requirements.txt
 echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖....🧞‍♂️"
 python3 bot.py
