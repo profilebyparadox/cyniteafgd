cb72255c299ffa3853a4d291222f505b6de24676
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2023-03-11 15:35:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -576,7 +576,7 @@ async def send_msg(bot, message):
     else:
         await message.reply_text("<b>Use this command as a reply to any message using the target chat id. For eg: /send userid</b>")
 
-@Client.on_message(filters.command("shortlink") & filters.user(ADMINS))
+@Client.on_message(filters.command("shortner") & filters.user(ADMINS))
 async def shortlink(bot, message):
     chat_type = message.chat.type
     if chat_type == enums.ChatType.PRIVATE:
